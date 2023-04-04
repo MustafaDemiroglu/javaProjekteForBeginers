62aa2f45b7c3691dfaa466133a9fb0495760671e
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-04-04 14:58:31 +0200

    Java learning at school

--- w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day01/C03_variables.java
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day01/C03_variables.java
@@ -1,4 +1,34 @@
 package Day01;
 
 public class C03_variables {
+    /* Java Variables
+    Variables are containers for storing data values.
+    In Java, there are different types of variables, for example:
+    String - stores text, such as "Hello". String values are surrounded by double quotes
+    int - stores integers (whole numbers), without decimals, such as 123 or -123
+    float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+    boolean - stores values with two states: true or false
+    */
+
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println (name) ;
+
+        int myNum = 15 ;
+        System.out.println (myNum) ;
+
+        int myNum2 = 15;
+        myNum2 = 20;  // myNum2 is now 20
+        System.out.println(myNum2);
+
+        float myFloatnum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true ;
+
+        System.out.println(myFloatnum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+
+    }
 }
