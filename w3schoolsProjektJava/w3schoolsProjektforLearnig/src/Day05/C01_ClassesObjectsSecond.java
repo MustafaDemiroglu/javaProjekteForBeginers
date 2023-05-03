5d00bd7f626d480f970da831c995738af05028ab
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-03 14:04:05 +0200

    method

---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day05/C01_ClassesObjectsSecond.java
@@ -0,0 +1,13 @@
+package Day05;
+
+public class C01_ClassesObjectsSecond {
+    public static void main(String[] args) {
+        C01_ClassesObjects myObj = new C01_ClassesObjects();      // Object 1
+        C01_ClassesObjects myObj2 = new C01_ClassesObjects();     // Object 2
+        C01_ClassesObjects myObj3 = new C01_ClassesObjects();     // Object 3
+
+        System.out.println(myObj.x);
+        System.out.println(myObj2.x);
+        System.out.println(myObj3.y);
+    }
+}
