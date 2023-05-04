9c778083c9d049c5eec726fc98ad242e401342d0
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-04 13:31:14 +0200

    Update D_05_ArrayList.java

--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_ArrayList.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_ArrayList.java
@@ -1,9 +1,33 @@
 package p_01_demo;
 
+import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
 public class D_05_ArrayList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		List einkauf = new ArrayList();
+		einkauf.add("Käse") ;
+		einkauf.add("Wurst") ;
+		einkauf.add("Brot") ;
+		einkauf.add(47) ;
+		
+		// einkauf.remove(0);
+		
+		for (int i=0; i<einkauf.size();i++) {
+			System.out.println(einkauf.get(i));
+		}
+		
+		if(einkauf.contains("Wurst")) {
+			System.out.println("Wurst ist an Position : " + einkauf.indexOf("Wurst"));
+			System.out.println("wurst ist enthalten");
+		} else {
+			System.out.println("Nicht in der Liste"); 
+		}
 
 	}
 
