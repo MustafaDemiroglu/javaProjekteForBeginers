42a33ee9928047982262fa033ac350ea2437c0fe
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-04 12:38:38 +0200

    array

---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_ArrayList.java
@@ -0,0 +1,10 @@
+package p_01_demo;
+
+public class D_05_ArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_Arrays3.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_Arrays3.java
@@ -1,10 +1,13 @@
 package p_01_demo;
 
+import java.util.Scanner;
+
 public class D_05_Arrays3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/* Erste Variante
 		String einkauf1 = "Äpfel" ;
 		String einkauf2 = "Milch" ;
 		String einkauf3 = "Brot" ;
@@ -14,8 +17,10 @@ public class D_05_Arrays3 {
 		System.out.println(einkauf2);
 		System.out.println(einkauf3);
 		System.out.println(einkauf4);
+		*/
 		
-		int anzahl = 5 ;
+		
+		/* 2. Variante
 		String[] einkaufListe = new String[anzahl];
 		einkaufListe[0] = "Äpfel" ;
 		einkaufListe[1] = "Milch" ;
@@ -23,8 +28,36 @@ public class D_05_Arrays3 {
 		einkaufListe[3] = "Wurst" ;
 		einkaufListe[4] = "Gemüse" ;
 		
-		System.out.println("hier ist mir Array und Schleife");
+		System.out.println("Hier ist mir Array und Schleife");
 		for (int i=0; i<anzahl; i++) {
+			System.out.println(einkaufListe[i]);
+		}
+		*/
+		
+		
+		/* Variante 3
+		System.out.println("Hier ist eine andere variante Array");
+		String[] einkaufZettel = {"Äpfelx", "Milchx", "Brotx", "Wurstx", "Gemüsex", "Kaffex"};
+		// einkaufZettel[6] = "Duschgel" ;       
+		// musste schon vorher die anzahl gegeben 
+		for (int i=0; i<anzahl; i++) {
+			System.out.println(einkaufZettel[i]);
+		}
+		
+		*/
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Anzahl");
+		int anzahl = scan.nextInt() ;
+		String[] einkaufListe= new String[anzahl] ; 
+		System.out.println("Test: " + einkaufListe.length);
+		
+		for (int i=0; i<anzahl; i++) {
+			System.out.println("Eingabe: " + i);
+			einkaufListe[i] = scan.nextLine();
+		}
+		
+		for(int i=0; i<einkaufListe.length; i++) {
 			System.out.println(einkaufListe[i]);
 		}
 
