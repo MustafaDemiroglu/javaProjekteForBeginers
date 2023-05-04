65122ff910ed856def777e05f1c469a93eb4451a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-04 14:00:42 +0200

    Schleife

---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_05_EinkaufsListe.java
@@ -0,0 +1,42 @@
+package p_01_demo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class D_05_EinkaufsListe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		List<String> einkaufsListe = new ArrayList<String>();
+		String einkauf ="";
+		boolean weiter = true ;
+		System.out.println("Erfasse deinen einkauf, beenden mit x");
+		
+		while (weiter) {
+			einkauf = scan.nextLine();
+			if(einkauf.equals("x")) {
+				System.out.println("Liste Fertig!");
+				weiter = false;
+			} else {
+				einkaufsListe.add(einkauf);
+			}
+		}
+		
+		for (int i=0;i<einkaufsListe.size();i++) {
+			System.out.println(einkaufsListe.get(i));
+		}
+		
+		Collections.sort(einkaufsListe);
+		System.out.println("--------------------------");
+		for (int i=0;i<einkaufsListe.size();i++) {
+			System.out.println(einkaufsListe.get(i));
+		}
+		
+
+	}
+
+}
---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_06_Schleife.java
@@ -0,0 +1,24 @@
+package p_01_demo;
+
+public class D_06_Schleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("------------ Scheliefe mit for-------------");
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("------------ Scheliefe mit while-------------");
+		int x=1;
+		while(x<=10) {
+			System.out.println(x);
+			x++;
+		}
+		
+		
+
+	}
+
+}
