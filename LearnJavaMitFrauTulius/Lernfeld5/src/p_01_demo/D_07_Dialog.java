7cd1e8c9d0047617cc570609f1857be33e669cd0
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-09 12:59:50 +0200

    ad

--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_07_Dialog.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_07_Dialog.java
@@ -3,19 +3,24 @@ package p_01_demo;
 import javax.swing.JOptionPane;
 
 public class D_07_Dialog {
-	
-	public static void main (String [] args) {
-		
-		// TODO
-		
-		JOptionPane.showMessageDialog(null, "Die Nachricht die angezeigt werden ist : ") ;
-		String eingabe = JOptionPane.showInputDialog("Ihr Benzinverbrauch : ") ;
-		eingabe = eingabe.replace("," , ".") ;
-		
-		
-		
-		
-		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "Die Nachricht, die angezeigt werden soll!");
+
+		String eingabe = JOptionPane.showInputDialog("Ihr Benzinverbrauch:");
+		eingabe = eingabe.replace(",", ".");
+
+		try {
+			// Integer.parseInt()
+			double verbrauch = Double.parseDouble(eingabe);
+
+			System.out.println(verbrauch);
+		} catch (Exception e) {
+			System.out.println("Fehler: " + e);
+		}
+
 	}
 
 }
