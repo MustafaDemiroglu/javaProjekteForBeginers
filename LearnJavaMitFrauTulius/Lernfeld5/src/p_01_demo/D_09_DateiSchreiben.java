fb38d9cc6be647f2fb92dde744884b9fea37d793
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-16 09:04:01 +0200

    fileWriter

--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_09_DateiSchreiben.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_09_DateiSchreiben.java
@@ -13,6 +13,10 @@ public class D_09_DateiSchreiben {
 			// Erzeugen einer TXT-Datei
 			fw = new FileWriter("Demo.txt");
 			fw.write("Dies ist unsere 1. _Datei");
+			fw.write("\n2.Versuch");
+			fw.write("\r3.Versuch");
+			fw.write("\nAdana Kebap");
+			fw.write("\n\rhier möchte ich zeigen wie man neue Reihe anfängt \nhilft uns");
 			fw.close();
 		} catch(Exception e) {
 			System.out.println("Fehler beim erstellen der Datei");
