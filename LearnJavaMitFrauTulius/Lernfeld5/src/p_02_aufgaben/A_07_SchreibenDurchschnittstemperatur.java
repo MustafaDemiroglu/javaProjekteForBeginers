2662bd6e79f07b88f26f63df61207bce9c1ae537
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-16 13:40:42 +0200

    csv

---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_02_aufgaben/A_07_SchreibenDurchschnittstemperatur.java
@@ -0,0 +1,58 @@
+/* Aufgabenstellung: Durchschnittstemperatur
+ * Der Benutzer soll nach Eingabe seines Wohnorts aufgefordert werden Temperaturen einzugeben
+ * Die Durchschnittstemperatur soll berechnet und dem Benutzer angezeigt werden. 
+ * Wenn Durchschnittstemperatur unter 20 Grad liegt Hinweis: „Noch kein
+ * Sommer“, ansonsten Hinweis: „Zeit für T-Shirt“
+ * Die Temperaturen sollen in einem Array gespeichert werden.Nutzen Sie eine Schleife zum Erfassen der Daten. 
+ * Der Benutzer soll nun auch die Möglichkeiten haben eine beliebige Anzahl an Temperaturen zu erfassen. Erfragen Sie vorher wie viele Werte er erfassen möchte.
+ * Nutzen Sie für die Interaktion mit dem Benutzer (Ein- und Ausgabe) die Klasse JOptionPane.
+ *
+ *Schreiben Sie folgende Methoden:
+ * eingabeTemperaturen(int anzahl): double[]
+ * berechneDurchschnitt(double[] temperaturen): double
+ *
+ * Erweiterung:
+ * Schreiben Sie die Methode feedbackAnzeigen um:
+ * feedbackAnzeigen(String ort, int anzahl, double durchschnitt): void
+ * feedbackErzeugen(String ort, int anzahl, double durchschnitt): String
+ * Schreiben Sie eine weitere Methode, die die Daten in eine Datei schreibt:
+ * dateiSchreiben(String feedback, String ort): void
+ * Der Ort soll als Dateiname dienen z.B. Heidelberg.txt
+*/
+
+
+
+/* 
+package p_02_aufgaben;
+
+import javax.swing.JOptionPane;
+
+
+public class A_07_SchreibenDurchschnittstemperatur {
+
+	public static double[] eingabeTemperaturen(int anzahl) {
+		
+		
+		
+		return temperaturen[];
+	}
+	
+	public static double berechneDurchschnitt (double[] temperaturen) {
+		
+		return durchschnitt;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		double [] temperaturen;
+		double durchschnitt = berechneDurchschnitt (temperaturen);
+		
+		JOptionPane.showMessageDialog(null, " Hallo allerzeit, wie rechnen jetzt zusammen die durchschnittliche Temperatur eurer Stadt.");
+		
+		String ort = JOptionPane.showInputDialog("Geben sie der Ort ein :" );
+		
+	}
+	
+}
+*/
