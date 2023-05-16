be4e038f206d0d322592b24a945c62ce0150034c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-16 09:33:57 +0200

    bw.write(array); 			bw.newLine();

--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_09_DateiGepuffert.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/D_09_DateiGepuffert.java
@@ -10,15 +10,35 @@ public class D_09_DateiGepuffert {
 		
 		try {
 			
-			FileWriter fw = new FileWriter("Demo2.txt");
+			// true als Parameter sorgt dafür, dass Dateiinhalt bestehen bleibt
+			//und neuer inhalt angehängt wird
+			FileWriter fw = new FileWriter("benutzer.txt",true);
 			// Zwischenspeichern "Bufffern"
 			BufferedWriter bw = new BufferedWriter(fw);
 			
-			bw.write("1. Zeile");
+			/*
 			bw.newLine();
-			bw.write("2. Zeile");
+			bw.write("7. Zeile");
+			//Erzeugt write.newline() Zeilumbruch
+			bw.newLine();
+			bw.write("8. Zeile");
+			*/
 			
+			String[]benutzer;
+			benutzer = new String[3];
+			benutzer[0] = "Hansi";
+			benutzer[1] = "Sabine";
+			benutzer[2] = "Daniel";
+			
+			for (int i=0;i<benutzer.length;i++) {
+				bw.write(benutzer[i]);
+				bw.newLine();
+			}
+			
+			
+			// Jetzt wird Zwischenspeicher in Datei geschrieben
 			bw.close();
+			// Schliesen der Datei
 			fw.close();
 		} catch (Exception e) {
 			// TODO: handle exception
