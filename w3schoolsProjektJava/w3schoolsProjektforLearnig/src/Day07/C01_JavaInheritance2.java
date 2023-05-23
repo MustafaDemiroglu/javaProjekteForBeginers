c19cf2aab46c0792cc90752343a82593e75b1774
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-23 13:45:41 +0200

    update

--- w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day07/C01_JavaInheritance2.java
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day07/C01_JavaInheritance2.java
@@ -1,13 +1,13 @@
 package Day07;
 
-public class C02_JavaInheritance2 extends C01_JavaInheritance {
+public class C01_JavaInheritance2 extends C01_JavaInheritance {
 
     private String modelName = "Mustang" ;      // Car attribute
 
     public static void main (String [] args) {
 
         // Create a myCar object
-        C02_JavaInheritance2 myCar = new C02_JavaInheritance2() ;
+        C01_JavaInheritance2 myCar = new C01_JavaInheritance2() ;
 
         // Call the honk() method (from the Vehicle Class) on the myCar Object
         myCar.honk();
