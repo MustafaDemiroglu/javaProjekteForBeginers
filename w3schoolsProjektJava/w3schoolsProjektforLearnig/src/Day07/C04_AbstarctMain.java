e2b0c46833c2ff897806f393852221542f1064cb
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-23 14:59:00 +0200

    abstract

---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day07/C04_AbstarctMain.java
@@ -0,0 +1,12 @@
+package Day07;
+
+public class C04_AbstarctMain {
+
+    public static void main(String[] args) {
+
+        C03_JavaPolymorphism_Pig myPig = new C03_JavaPolymorphism_Pig();    // Create a Pig Object
+
+        myPig.animalSound();
+        // myPig.sleep();
+    }
+}
---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day07/C04_Abstract_Animal.java
@@ -0,0 +1,14 @@
+package Day07;
+
+// Abstract class
+abstract class C04_Abstract_Animal {
+
+    // abstract method (does not have a body)
+    public abstract void animalSound();
+
+    // regular Method
+    public void sleep() {
+        System.out.println("Zzzzz");
+    }
+
+}
