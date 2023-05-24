48350b1953d98a00f7c57c70d5b3d66fbd80f851
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-24 13:03:53 +0200

    Create P_04_TaschenRechnerFrontend.java

---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_03_projekte/P_04_TaschenRechnerFrontend.java
@@ -0,0 +1,39 @@
+package p_03_projekte;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class P_04_TaschenRechnerFrontend extends JFrame {
+   JTextField tf;
+   Container container;
+   JLabel label;
+   public JTextFieldValidation() {
+      container = getContentPane();
+      setBounds(0, 0, 500, 300);
+      tf = new JTextField(25);
+      setLayout(new FlowLayout());
+      container.add(new JLabel("Enter the number"));
+      container.add(tf);
+      container.add(label = new JLabel());
+      label.setForeground(Color.red);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      tf.addKeyListener(new KeyAdapter() {
+         public void keyPressed(KeyEvent ke) {
+            String value = tf.getText();
+            int l = value.length();
+            if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') {
+               tf.setEditable(true);
+               label.setText("");
+            } else {
+               tf.setEditable(false);
+               label.setText("* Enter only numeric digits(0-9)");
+            }
+         }
+      });
+      setVisible(true);
+   }
+   public static void main(String[] args) {
+      new JTextFieldValidation();
+   }
+}
