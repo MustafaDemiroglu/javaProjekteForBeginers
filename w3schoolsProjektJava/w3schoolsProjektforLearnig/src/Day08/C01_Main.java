09ea1c08a6e689face254c87e05e13aa62ef7c72
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-25 14:44:00 +0200

    Update

---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day08/C01_Main.java
@@ -0,0 +1,11 @@
+package Day08;
+
+public class C01_Main {
+
+    public static void main(String[] args) {
+
+        C01_DemoClass myObj = new C01_DemoClass();
+        myObj.myMethod();
+        myObj.myOtherMethod();
+    }
+}
