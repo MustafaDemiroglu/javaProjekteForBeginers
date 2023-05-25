a9c0d650531d46737cf97b85436ffd1b91162ac5
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-25 14:33:16 +0200

    Interface

---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day08/C01_JavaInterfaceMain.java
@@ -0,0 +1,11 @@
+package Day08;
+
+public class C01_JavaInterfaceMain {
+
+    public static void main(String[] args) {
+        C01_JavaInterfacePig myPig = new C01_JavaInterfacePig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+
+}
---
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day08/C01_JavaInterfacePig.java
@@ -0,0 +1,12 @@
+package Day08;
+
+public class C01_JavaInterfacePig implements C01_JavaInterface {
+
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep() {
+        System.out.println("Zzz");
+    }
+
+}
