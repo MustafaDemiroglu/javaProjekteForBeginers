8f525955ec5e8145aaf6bc30490a82205cdd00ce
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-05 09:06:31 +0200

    str

--- LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/S_01_Zinsen.java
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/S_01_Zinsen.java
@@ -1,9 +1,30 @@
 package p_01_demo;
 
+import java.util.Scanner;
+
 public class S_01_Zinsen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		double K;
+		double K1;
+		double p;
+		int Jahr=0;
+		
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Kapital : ");
+		K= scan.nextDouble();
+		K1 = K;
+		System.out.println("Zinssatz : ");
+		p = scan.nextDouble();
+		
+		do {
+			K1 = K1 * (1+p/100);
+			Jahr++ ;
+		} while (K1<K*2);
+		
+		System.out.println(Jahr);
 
 	}
 
