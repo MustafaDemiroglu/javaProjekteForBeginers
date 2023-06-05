85a00e24c3e3b48916d826b59b685f0e9c8e67ee
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-05 08:52:15 +0200

    struktogram

---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_01_demo/S_01_Zinsen.java
@@ -0,0 +1,10 @@
+package p_01_demo;
+
+public class S_01_Zinsen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LearnJavaMitFrauTulius/Lernfeld5/src/p_02_aufgaben/A_08_StruktogrammBeiSpiel.java
@@ -0,0 +1,19 @@
+package p_02_aufgaben;
+
+import javax.swing.JOptionPane;
+
+public class A_08_StruktogrammBeiSpiel {
+	
+	public static void main (String [] args) {
+		
+		JOptionPane.showMessageDialog(null, "Das ist ein Beispiel für Struktogram");
+		
+		double K = Double.parseDouble(JOptionPane.showInputDialog("Geben sie die Wert von K ein : "));
+		double K1 = K;
+		
+		
+		
+		
+	} 
+
+}
