fafb083f3ac045713d3a7b8ecda7c02dc2cb7bc5
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-06 14:46:06 +0200

    Array

--- w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day09/C03_ArrayList.java
+++ w3schoolsProjektJava/w3schoolsProjektforLearnig/src/Day09/C03_ArrayList.java
@@ -1,4 +1,31 @@
 package Day09;
 
+import java.util.ArrayList;
+
 public class C03_ArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> autos = new ArrayList<String>();
+        autos.add("Mercedes");
+        autos.add("Audi");
+        autos.add("Volvo");
+        System.out.println(autos);
+        System.out.println("----------------");
+
+        autos.add("BMW");
+        autos.add("Ford");
+        System.out.println(autos);
+        System.out.println("-------------------");
+
+        autos.add("Mazda");
+        System.out.println(autos);
+        System.out.println("--------------");
+
+        autos.remove("Audi");
+        autos.remove("Ford");
+        autos.remove("Mazda");
+        System.out.println(autos);
+        System.out.println("-----------------");
+    }
 }
