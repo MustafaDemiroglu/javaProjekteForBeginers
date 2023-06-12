87b14358aca7c78d085bcb92d6062a20ff4d2e64
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-12 15:03:55 +0200

    ogg

--- LearnJavaMitFrauTulius/Lernfeld8/src/Fuhrpark/Auto.java
+++ LearnJavaMitFrauTulius/Lernfeld8/src/Fuhrpark/Auto.java
@@ -1,5 +1,52 @@
 package Fuhrpark;
 
 public class Auto {
+	
+	// Attribut(e)
+	private String hersteller = "VW" ;
+	private String farbe = "schwarz" ;
+	private int kilometerstand = 0 ;
+	
+	
+	// Methode(n)
+	// automatisch generierte getter und setter 
+	public String getFarbe() {
+		return farbe;
+	}
+	
+	public void setFarbe(String farbe) {
+		System.out.println("Auto wird umlackiert");
+		this.farbe = farbe;
+	}
+	
+	// eigene Methoden
+	public void fahren (int kilometer) {
+		// welcher kilometer
+		// Kilometerstabd aktualisieren
+		kilometerstand = kilometerstand+kilometer;
+		// kilometerstand+=kilometer;
+	}
+	
+	public void sagHersteller() {
+		System.out.println(hersteller);
+	}
+	
+	
+	
+	// Konstruktor(en)
+	public Auto() {
+		System.out.println("Standardkonstruktor ");
+	}
+	
+	public Auto (String hersteller) {
+		this.hersteller= hersteller ;
+		System.out.println("Konstruktor 1");
+	}
+	
+	public Auto (String hersteller, String farbe) {
+		this.hersteller = hersteller ;
+		this.farbe = farbe ;
+		System.out.println("Konstruktor 2");
+	} 
 
 }
--- LearnJavaMitFrauTulius/Lernfeld8/src/Fuhrpark/Garage.java
+++ LearnJavaMitFrauTulius/Lernfeld8/src/Fuhrpark/Garage.java
@@ -1,5 +1,31 @@
 package Fuhrpark;
 
 public class Garage {
-
+	
+	public static void  main (String [] args) {
+		System.out.println("Willkommen in der Garage");
+		
+		// Objekte erzeugen
+		// Standardkonstruktor
+		
+		Auto standardAuto = new Auto();
+		standardAuto.sagHersteller();
+		System.out.println(standardAuto.getFarbe());
+		
+		standardAuto.setFarbe("lila");
+		System.out.println(standardAuto.getFarbe());
+		
+		Auto auto1 = new Auto ("Peugeot") ;
+		auto1.sagHersteller();
+		System.out.println(auto1.getFarbe());
+		
+		Auto auto2 = new Auto("Mercedes", "silber");
+		auto2.sagHersteller();
+		System.out.println(auto2.getFarbe());
+		
+		Auto auto3 = new Auto ("Opel"); 
+		auto3.sagHersteller();
+		
+	}
 }
+
