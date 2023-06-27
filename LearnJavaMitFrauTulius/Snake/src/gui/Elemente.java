7de90705f2f12c32a133017f1271a31060a0d318
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-27 10:28:56 +0200

    Schlange Game

--- LearnJavaMitFrauTulius/Snake/src/gui/Elemente.java
+++ LearnJavaMitFrauTulius/Snake/src/gui/Elemente.java
@@ -17,9 +17,24 @@ public class Elemente extends JLabel {
 		// Hintergrund Spielfeld
 		g.setColor(Color.BLACK);
 		g.fillRect(Gui.xoff, Gui.yoff, 512, 512);
+		
 		// Raster/Grid Spielfeld (16*16 Felder)
+		g.setColor(Color.WHITE);
+		for (int x=0;x<16;x++) {
+			g.drawRect(Gui.xoff + x*32 , Gui.yoff, 32, 32);
+			for (int y=0; y<16 ;y++) {
+				g.drawRect(Gui.xoff + x*32 , Gui.yoff + y *32 , 32, 32);
+			} 
+		}
+		
+		
+		
 		
 		// Rand Spielfeld
+		g.setColor(Color.RED);
+		g.drawRect(Gui.xoff, Gui.yoff, 512, 512);
+		
+		
 		
 		// Schlange
 		
