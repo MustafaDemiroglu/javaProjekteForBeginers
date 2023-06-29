91ff8a6f447214c0ca1da7ae13ca3ee6e052a752
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-29 12:19:46 +0200

    Update Frucht.java

--- LearnJavaMitFrauTulius/Snake/src/game/Frucht.java
+++ LearnJavaMitFrauTulius/Snake/src/game/Frucht.java
@@ -1,5 +1,37 @@
 package game;
 
 public class Frucht {
+	
+	int x ;
+	int y;
+	
+	public Frucht() {
+		// Zufallzahlen für x und y
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	// Frucht neu positionieren wenn gefuttert oder nach bestimmter Zeit
+	public void reset( ) {
+		
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
 
 }
