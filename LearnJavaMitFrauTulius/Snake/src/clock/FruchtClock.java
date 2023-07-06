4fb65136a316c40e3b4cb8006e555363b2f6e72c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-06 09:23:42 +0200

    Update FruchtClock.java

--- LearnJavaMitFrauTulius/Snake/src/clock/FruchtClock.java
+++ LearnJavaMitFrauTulius/Snake/src/clock/FruchtClock.java
@@ -6,30 +6,6 @@ import gui.Gui;
 
 public class FruchtClock extends Thread {
 	
-	private int x;
-	private int y;
-	private Random random;
-	
-	public FruchtClock() {
-		random = new Random();
-		// Frucht reseten
-		resetFrucht();
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y; 
-	}
-	
-	public void resetFrucht() {
-		// Generiere zufällige Koordinaten für die Frucht
-		x = random.nextInt(Gui.width);
-		y = random.nextInt(Gui.height);
-	}
-	
 	
 
 }
