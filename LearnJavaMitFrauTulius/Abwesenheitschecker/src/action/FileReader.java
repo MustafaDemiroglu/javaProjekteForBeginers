12e591eb5c2bfb6b22624310ee53dc0d93d7c782
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-08-24 13:51:45 +0200

    neue projekt

---
+++ LearnJavaMitFrauTulius/Abwesenheitschecker/src/action/FileReader.java
@@ -0,0 +1,14 @@
+package action;
+
+import javax.swing.JfileChooser;
+
+public class FileReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFileChooser chooser = new JFileChooser();
+
+	}
+
+}
