64c7a03661eb5f896a7201d5c802dd1300f2d93f
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-08-28 11:52:24 +0200

    Update AbwesenheitsChecker.java

--- LearnJavaMitFrauTulius/Abwesenheitschecker/src/action/AbwesenheitsChecker.java
+++ LearnJavaMitFrauTulius/Abwesenheitschecker/src/action/AbwesenheitsChecker.java
@@ -1,42 +1,51 @@
 package action;
 
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JFilechooser;
 
 public class AbwesenheitsChecker {
+    public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
 
-	static JFileChooser chooser;
-	static String school;
-	static String teams;
-	List klassenliste;
-	List teamsliste; 
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		school = ChooseFile(school);
-		System.out.println(school);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
 
-	}
-	
-	public static String ChooseFile() {
-		
-		try {
-			chooser = new JFileChooser();
-			chooser.showOpendialog(null);
-			chooser.getSelectedfile(output);
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} 
-		
-		// return chooser
-		return selectedFile;
-	}
-	
-	public void readFile() {
-		
-	}
+            List<String> klassenList = readCSV(selectedFile.getAbsolutePath());
+            List<String> teilnehmerList = readCSV(selectedFile.getAbsolutePath());
 
+            List<String> abwesendList = new ArrayList<>();
+            for (String teilnehmer : teilnehmerList) {
+                if (!teilnehmerList.contains(teilnehmerList)) {
+                    abwesendList.add(teilnehmer);
+                }
+            }
+
+            System.out.println(abwesendList);
+
+            try (FileWriter writer = new FileWriter("abwesenheitsList.csv")) {
+                for (String abwesend : abwesendList) {
+                    writer.write(abwesend + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static List<String> readCSV(String filename) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
 }
